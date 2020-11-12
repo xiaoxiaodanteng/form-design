49705b153097b3ecb339c7f707884354da4a504a
qinsihui <qinsihui@paisheng.com>
2020-11-12 16:29:43 +0800

    更新获取远程数据接口

--- graphqlRequest.js
+++ graphqlRequest.js
@@ -7,7 +7,8 @@ const headers = {
 
 // create an axios instance
 const service = axios.create({
-  baseURL: `${window.origin}${process.env.VUE_APP_BASE_API}/api/data-workflow-process-erp`,
+  // baseURL: `${window.origin}${process.env.VUE_APP_BASE_API}/api/data-workflow-process-erp`,
+  baseURL: `http://bi.dev.nearbyexpress.com/api/data-workflow-process-erp`,
   timeout: 50000 // request timeout
 })
 service.defaults.headers = headers
