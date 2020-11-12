d7e598c453c8d16ddd25ea8a25e47cffce8615ab
qinsihui <qinsihui@paisheng.com>
2020-11-12 16:23:29 +0800

    发布v1.0.6

--- graphqlRequest.js
+++ graphqlRequest.js
@@ -7,7 +7,7 @@ const headers = {
 
 // create an axios instance
 const service = axios.create({
-  baseURL: `${process.env.VUE_APP_BASE_API}/api/data-workflow-process-erp`,
+  baseURL: `${window.origin}${process.env.VUE_APP_BASE_API}/api/data-workflow-process-erp`,
   timeout: 50000 // request timeout
 })
 service.defaults.headers = headers
