48feb309d63407394cbec92903ccc20b8571171b
qinsihui <qinsihui@paisheng.com>
2020-12-07 16:25:48 +0800

    update

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const ParserPlugin = {
 ParserPlugin.install = (Vue, options = { hostname: 'http://bi.dev.nearbyexpress.com' }) => {
   Vue.component(Parser.name, Parser)
 
-  Vue.prototype.$hostname = options.hostname
   ParserPlugin.$Vue = Vue
+  ParserPlugin.$Vue.prototype.$hostname = options.hostname
+  console.log(options, ParserPlugin.$Vue.prototype)
   // 自动注册组件
   componentFiles.keys().forEach((filePath, index) => {
     const componentName = filePath.replace(/^\.\/(.*)\.\w+$/, '$1')
