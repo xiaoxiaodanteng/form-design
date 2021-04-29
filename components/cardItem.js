50f61f6c5884ea45efac198e0ac3b7c70c71114d
qinsihui <qinsihui@paisheng.com>
2021-04-29 10:24:24 +0800

    v2.1.1

--- components/cardItem.js
+++ components/cardItem.js
@@ -36,9 +36,13 @@ export default {
     const config = this.scheme.__config__
     if (!config.show) return null
     const child = this.parser.renderChildren(h, this.scheme.__config__.children)
+    const headerStyle = config.headerStyle || {
+      textAlign: config.align,
+      fontSize: '14px'
+    }
     return h('el-col', { attrs: { span: config.span }}, [
       h('el-card', { attrs: { bodyStyle: scheme['body-style'] ? JSON.parse(scheme['body-style']) : { padding: 0 }}}, [
-        h('div', { style: { textAlign: config.align }, slot: 'header' }, config.label),
+        h('div', { style: headerStyle, slot: 'header' }, config.label),
         h('el-row', [child])
       ]),
 
